4938f63426f4415d09a3d643dd7853c373cdab34
tumtumtum <tumtumtum@gmail.com>
2014-01-15 16:45:18 +0000

    Added support for using System.Data.Sqlite on Mono by using alternate build that references native libsqlite3.so

--- src/Shaolinq.Sqlite/SqliteSqlDatabaseContextInfo.cs
+++ src/Shaolinq.Sqlite/SqliteSqlDatabaseContextInfo.cs
@@ -1,6 +1,11 @@
 // Copyright (c) 2007-2013 Thong Nguyen (tumtumtum@gmail.com)
 
 using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading;
 using Platform.Xml.Serialization;
 ﻿using Shaolinq.Persistence;
 ﻿
@@ -13,6 +18,8 @@ namespace Shaolinq.Sqlite
 		[XmlAttribute]
 		public string FileName { get; set; }
 
+		private static int MonoLibraryAlreadyLoaded;
+
 		public override SqlDatabaseContext CreateSqlDatabaseContext(DataAccessModel model)
 		{
 			var useMonoData = Environment.GetEnvironmentVariable("SHAOLINQ_USE_MONO_DATA_SQLITE");
@@ -23,6 +30,34 @@ namespace Shaolinq.Sqlite
 			}
 			else
 			{
+				if (SqliteSqlDatabaseContext.IsRunningMono())
+				{
+					if (Interlocked.CompareExchange(ref MonoLibraryAlreadyLoaded, 1, 0) == 0)
+					{
+						var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+						var assembly = assemblies.FirstOrDefault(c => c.GetName().Name == "System.Data.SQLite");
+
+						if (assembly == null)
+						{
+							byte[] bytes;
+							var codeBase = new Uri(this.GetType().Assembly.CodeBase);
+
+							if (string.Equals(codeBase.Scheme, "file", StringComparison.InvariantCultureIgnoreCase))
+							{
+								var fileName = Path.Combine(Path.GetDirectoryName(codeBase.LocalPath), "Mono", "System.Data.SQLite.Mono.dll");
+
+								bytes = File.ReadAllBytes(fileName);
+							}
+							else
+							{
+								throw new NotSupportedException("Sqlite Provider requires application to be located on a local file system");
+							}
+
+							Assembly.Load(bytes);
+						}
+					}
+				}
+
 				return SqliteWindowsSqlDatabaseContext.Create(this, model);
 			}
 		}
