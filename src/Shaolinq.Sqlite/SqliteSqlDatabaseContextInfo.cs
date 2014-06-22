823e62516d00bb3799df004eb9baaea2e15743c9
tumtumtum <tumtumtum@gmail.com>
2014-06-22 23:09:31 +0100

    Removed sqlite_config  SQLITE_CONFIG_SERIALIZED from Shaolinq.Sqlite

--- src/Shaolinq.Sqlite/SqliteSqlDatabaseContextInfo.cs
+++ src/Shaolinq.Sqlite/SqliteSqlDatabaseContextInfo.cs
@@ -11,47 +11,13 @@ namespace Shaolinq.Sqlite
 	public class SqliteSqlDatabaseContextInfo
 		: SqlDatabaseContextInfo
 	{
-		private static class NativeMethods
-		{
-			internal const int SQLITE_CONFIG_SERIALIZED = 3;
-
-			[DllImport("sqlite3", CallingConvention = CallingConvention.Cdecl, EntryPoint = "sqlite3_config")]
-			internal static extern int sqlite3_config_none(int op);
-		}
-
-		private static readonly bool useMonoData;
-
-		static SqliteSqlDatabaseContextInfo()
-		{
-			useMonoData = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SHAOLINQ_USE_MONO_DATA_SQLITE"));
-			var nonSerialized = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SHAOLINQ_SQLITE_NONSERIALIZED"));
-
-			if (SqliteSqlDatabaseContext.IsRunningMono())
-			{
-				if (!useMonoData && !nonSerialized)
-				{
-					try
-					{
-						var result = NativeMethods.sqlite3_config_none(NativeMethods.SQLITE_CONFIG_SERIALIZED);
-
-						if (result != 0)
-						{
-							Console.Error.WriteLine("Warning: Could not configure native sqlite library to run in serialized mode. Result=" + result);
-						}
-					}
-					catch
-					{
-						Console.Error.WriteLine("Warning: Could not configure native sqlite library to run in serialized mode");
-					}
-				}
-			}
-		}
-
 		[XmlAttribute]
 		public string FileName { get; set; }
 
 		public override SqlDatabaseContext CreateSqlDatabaseContext(DataAccessModel model)
 		{
+			var useMonoData = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SHAOLINQ_USE_MONO_DATA_SQLITE"));
+
 			if (useMonoData && SqliteSqlDatabaseContext.IsRunningMono())
 			{
 				return SqliteMonoSqlDatabaseContext.Create(this, model);
