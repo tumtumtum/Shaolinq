15c87887d238cc1a71e36a162c6d0f1d2558fe92
tumtumtum <tumtumtum@gmail.com>
2014-10-26 17:00:22 +0000

    Added missing SqlPragmaExpression.cs file. Oops

---
+++ src/Shaolinq/Persistence/Linq/Expressions/SqlPragmaExpression.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2007-2014 Thong Nguyen (tumtumtum@gmail.com)
+
+using System.Linq.Expressions;
+
+namespace Shaolinq.Persistence.Linq.Expressions
+{
+	public class SqlPragmaExpression
+		: SqlBaseExpression
+	{
+		public string Directive { get; private set; }
+		public override ExpressionType NodeType { get { return (ExpressionType)SqlExpressionType.Pragma; } }
+
+		public SqlPragmaExpression(string directive)
+			: base(typeof(void))
+		{
+			this.Directive = directive;
+		}
+	}
+}
