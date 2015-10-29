0b9a6aa278ec03ab8692e198ddadfddf99802954
Thong Nguyen <tumtumtum@gmail.com>
2015-10-29 15:13:18 +0000

    More expression support for ExpressionInterpreter

--- src/Shaolinq/Persistence/Linq/Expressions/ExpressionInterpreter.cs
+++ src/Shaolinq/Persistence/Linq/Expressions/ExpressionInterpreter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 using Platform;
@@ -7,7 +8,7 @@ namespace Shaolinq.Persistence.Linq.Expressions
 {
 	public class ExpressionInterpreter
 	{
-		protected static object interpretFailed = new object();
+		protected static readonly object InterpretFailed = new object();
 
 		public static object Interpret(Expression expression)
 		{
@@ -15,7 +16,7 @@ namespace Shaolinq.Persistence.Linq.Expressions
 
 			var result = interpreter.Visit(expression);
 			
-			if (result == interpretFailed)
+			if (result == InterpretFailed)
 			{
 				result = ExpressionFastCompiler.CompileAndRun(expression);
 			}
@@ -27,6 +28,10 @@ namespace Shaolinq.Persistence.Linq.Expressions
 		{
 			switch (expression.NodeType)
 			{
+			case ExpressionType.New:
+				return this.Visit((NewExpression)expression);
+			case ExpressionType.MemberInit:
+				return this.Visit((MemberInitExpression)expression);
 			case ExpressionType.Convert:
 				return this.Visit((UnaryExpression)expression);
 			case ExpressionType.MemberAccess:
@@ -40,9 +45,11 @@ namespace Shaolinq.Persistence.Linq.Expressions
 				return this.Visit((MethodCallExpression)expression);
 			case ExpressionType.Constant:
 				return this.Visit((ConstantExpression)expression);
+			case ExpressionType.Equal:
+				return this.Visit((BinaryExpression)expression);
 			}
 
-			return interpretFailed;
+			return InterpretFailed;
 		}
 
 		protected object Visit(ConstantExpression expression)
@@ -50,14 +57,96 @@ namespace Shaolinq.Persistence.Linq.Expressions
 			return expression.Value;
 		}
 
+		protected object Visit(MemberInitExpression expression)
+		{
+			var obj = this.Visit(expression.NewExpression);
+
+			if (obj == InterpretFailed)
+			{
+				return obj;
+			}
+
+			foreach (var binding in expression.Bindings)
+			{
+				var value = this.Visit(obj, binding);
+
+				if (value == InterpretFailed)
+				{
+					return value;
+				}
+			}
+
+			return obj;
+		}
+
+		protected object Visit(object obj, MemberBinding binding)
+		{
+			switch (binding.BindingType)
+			{
+			case MemberBindingType.Assignment:
+				var assignment = ((MemberAssignment)binding);
+
+				var value = this.Visit(assignment.Expression);
+
+				if (value == InterpretFailed)
+				{
+					return value;
+				}
+
+				var fieldInfo = assignment.Member as FieldInfo;
+
+				if (fieldInfo != null)
+				{
+					fieldInfo.SetValue(obj, value);
+
+					return binding;
+				}
+
+				var propertyInfo = assignment.Member as PropertyInfo;
+
+				if (propertyInfo != null)
+				{
+					propertyInfo.SetValue(obj, value, null);
+
+					return binding;
+				}
+
+				return InterpretFailed;
+			}
+
+			return InterpretFailed;
+		}
+
+
+		protected object Visit(NewExpression expression)
+		{
+			var args = new object[expression.Arguments.Count];
+			
+			var i = 0;
+
+			foreach (var arg in expression.Arguments)
+			{
+				var reflected = this.Visit(arg);
+
+				if (reflected == InterpretFailed)
+				{
+					return reflected;
+				}
+
+				args[i++] = reflected;
+			}
+
+			return expression.Constructor.Invoke(args);
+		}
+
 		protected object Visit(MethodCallExpression expression)
 		{
 			var args = new object[expression.Arguments.Count];
 			var parentValue = expression.Object != null ? this.Visit(expression.Object) : null;
 
-			if (parentValue == interpretFailed)
+			if (parentValue == InterpretFailed)
 			{
-				return interpretFailed;
+				return InterpretFailed;
 			}
 
 			var i = 0;
@@ -66,9 +155,9 @@ namespace Shaolinq.Persistence.Linq.Expressions
 			{
 				var reflected = this.Visit(arg);
 
-				if (reflected == interpretFailed)
+				if (reflected == InterpretFailed)
 				{
-					return interpretFailed;
+					return InterpretFailed;
 				}
 
 				args[i++] = reflected;
@@ -81,28 +170,93 @@ namespace Shaolinq.Persistence.Linq.Expressions
 		{
 			switch (expression.NodeType)
 			{
-			case ExpressionType.Add:
-				if (expression.Type == typeof(string))
+			case ExpressionType.Equal:
+			case ExpressionType.NotEqual:
+			{
+				if (expression.Type == typeof(bool))
 				{
 					var left = this.Visit(expression.Left);
 
-					if (left == interpretFailed)
+					if (left == InterpretFailed)
 					{
-						return interpretFailed;
+						return InterpretFailed;
 					}
 
 					var right = this.Visit(expression.Right);
 
-					if (right == interpretFailed)
+					if (right == InterpretFailed)
 					{
-						return interpretFailed;
+						return InterpretFailed;
 					}
 
-					return string.Concat(left, right);
+					if (left == right)
+					{
+						return expression.NodeType == ExpressionType.Equal;
+					}
+
+					if (left is short && right is short)
+					{
+						return ((short)left == (short)right) && expression.NodeType == ExpressionType.Equal;
+					}
+					else if (left is int && right is int)
+					{
+						return (int)left == (int)right && expression.NodeType == ExpressionType.Equal;
+					}
+					else if (left is long && right is long)
+					{
+						return (long)left == (long)right && expression.NodeType == ExpressionType.Equal;
+					}
+					else if (left is string && right is string)
+					{
+						return (string)left == (string)right && expression.NodeType == ExpressionType.Equal;
+					}
+					else if (expression.Left.Type == typeof(object) && expression.Right.Type == typeof(object))
+					{
+						return EqualityComparer<object>.Default.Equals(left, right);
+					}
+
+					return InterpretFailed;
 				}
-				return interpretFailed;
+
+				return InterpretFailed;
+			}
+			case ExpressionType.Add:
+			{
+				var left = this.Visit(expression.Left);
+
+				if (left == InterpretFailed)
+				{
+					return InterpretFailed;
+				}
+
+				var right = this.Visit(expression.Right);
+
+				if (right == InterpretFailed)
+				{
+					return InterpretFailed;
+				}
+
+				if (left is short && right is short)
+				{
+					return ((short)left + (short)right);
+				}
+				else if (left is int && right is int)
+				{
+					return (int)left + (int)right;
+				}
+				else if (left is long && right is long)
+				{
+					return (long)left + (long)right;
+				}
+				else if (left is string || right is string)
+				{
+					return Convert.ToString(left)  + Convert.ToString(right);
+				}
+
+				return InterpretFailed;
+			}
 			default:
-				return interpretFailed;
+				return InterpretFailed;
 			}
 		}
 
@@ -110,7 +264,7 @@ namespace Shaolinq.Persistence.Linq.Expressions
 		{
 			var parentValue = expression.Expression != null ? this.Visit(expression.Expression) : null;
 
-			if (parentValue == interpretFailed)
+			if (parentValue == InterpretFailed)
 			{
 				return expression;
 			}
@@ -129,26 +283,26 @@ namespace Shaolinq.Persistence.Linq.Expressions
 				return propertyInfo.GetValue(parentValue, null);
 			}
 
-			return interpretFailed;
+			return InterpretFailed;
 		}
 
 		protected object Visit(UnaryExpression expression)
 		{
 			if (expression.Method != null)
 			{
-				return interpretFailed;
+				return InterpretFailed;
 			}
 
 			if (expression.NodeType != ExpressionType.Convert)
 			{
-				return interpretFailed;
+				return InterpretFailed;
 			}
 
 			var result = this.Visit(expression.Operand);
 
-			if (result == interpretFailed)
+			if (result == InterpretFailed)
 			{
-				return interpretFailed;
+				return InterpretFailed;
 			}
 
 			var underlyingType = expression.Type.GetUnwrappedNullableType();
