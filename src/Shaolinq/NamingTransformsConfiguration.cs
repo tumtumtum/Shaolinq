1ccf1bb8685648db21600957a962b59a2cfeeca8
Thong Nguyen <tumtumtum@gmail.com>
2015-12-11 16:57:44 +0000

    Added missing [XmlAttribute] on NamingTransformsConfiguration

--- src/Shaolinq/NamingTransformsConfiguration.cs
+++ src/Shaolinq/NamingTransformsConfiguration.cs
@@ -7,9 +7,16 @@ namespace Shaolinq
 	[XmlElement]
 	public class NamingTransformsConfiguration
 	{
+		[XmlAttribute]
 		public string DataAccessObjectName { get; set; }
+
+		[XmlAttribute]
 		public string PersistedMemberName { get; set; }
+
+		[XmlAttribute]
 		public string PersistedMemberPrefixName { get; set; }
+
+		[XmlAttribute]
 		public string PersistedMemberSuffixName { get; set; }
 	}
 }
