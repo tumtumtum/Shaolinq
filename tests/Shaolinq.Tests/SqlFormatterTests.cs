cd9de1ab8942c8ed97a7e557af32edaf37060d31
Thong Nguyen <tumtumtum@gmail.com>
2016-01-15 03:36:04 +0000

    Ignore SqlFormatterTests on Mono

--- tests/Shaolinq.Tests/SqlFormatterTests.cs
+++ tests/Shaolinq.Tests/SqlFormatterTests.cs
@@ -15,7 +15,7 @@ namespace Shaolinq.Tests
 	//[TestFixture("Postgres")]
 	//[TestFixture("Postgres.DotConnect")]
 	//[TestFixture("Postgres.DotConnect.Unprepared")]
-	[TestFixture("Sqlite")]
+	[TestFixture("Sqlite", Category = "IgnoreOnMono")]
 	//[TestFixture("SqliteInMemory")]
 	//[TestFixture("SqliteClassicInMemory")]
 	public class SqlFormatterTests
