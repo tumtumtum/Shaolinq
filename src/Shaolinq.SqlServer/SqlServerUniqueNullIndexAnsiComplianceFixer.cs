c62ea27f107f59c7b58bec575fcc769d2ae7b592
Thong Nguyen <tumtumtum@gmail.com>
2016-03-30 14:05:24 +0100

    Fixed tab/spacing in source

--- src/Shaolinq.SqlServer/SqlServerUniqueNullIndexAnsiComplianceFixer.cs
+++ src/Shaolinq.SqlServer/SqlServerUniqueNullIndexAnsiComplianceFixer.cs
@@ -5,22 +5,22 @@ using Shaolinq.Persistence.Linq.Expressions;
 
 namespace Shaolinq.SqlServer
 {
-    public class SqlServerUniqueNullIndexAnsiComplianceFixer
-        : SqlExpressionVisitor
-    {
-        public static Expression Fix(Expression expression)
-        {
-            return new SqlServerUniqueNullIndexAnsiComplianceFixer().Visit(expression);
-        }
+	public class SqlServerUniqueNullIndexAnsiComplianceFixer
+	: SqlExpressionVisitor
+	{
+		public static Expression Fix(Expression expression)
+		{
+			return new SqlServerUniqueNullIndexAnsiComplianceFixer().Visit(expression);
+		}
 
-        protected override Expression VisitCreateIndex(SqlCreateIndexExpression createIndexExpression)
-        {
-	        var predicate = createIndexExpression
-		        .Columns
-		        .Select(c => (Expression)new SqlFunctionCallExpression(typeof(bool), SqlFunction.IsNotNull, c.Column))
-		        .Aggregate(Expression.And);
+		protected override Expression VisitCreateIndex(SqlCreateIndexExpression createIndexExpression)
+		{
+			var predicate = createIndexExpression
+				.Columns
+				.Select(c => (Expression)new SqlFunctionCallExpression(typeof(bool), SqlFunction.IsNotNull, c.Column))
+				.Aggregate(Expression.And);
 
 			return createIndexExpression.ChangeWhere(createIndexExpression.Where == null ? predicate : Expression.And(createIndexExpression.Where, predicate));
-        }
-    }
+		}
+	}
 }
