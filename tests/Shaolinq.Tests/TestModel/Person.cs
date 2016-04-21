fbad6ea02a719e807da83b1df64ae718e0d082e8
Thong Nguyen <tumtumtum@gmail.com>
2016-04-21 01:07:48 +0100

    Minor tweak to unit test

--- tests/Shaolinq.Tests/TestModel/Person.cs
+++ tests/Shaolinq.Tests/TestModel/Person.cs
@@ -55,7 +55,7 @@ namespace Shaolinq.Tests.TestModel
 		[DependsOnProperty(nameof(Id))]
 		protected virtual string CompactIdString => this.Id.ToString("N");
 		
-		[ComputedTextMember("urn:$(PERSISTED_TYPENAME:L):{Id:N}")]
+		[ComputedTextMember("urn:$(TABLENAME:L):{Id:N}")]
 		public abstract string Urn { get; set; }
 	}
 }
