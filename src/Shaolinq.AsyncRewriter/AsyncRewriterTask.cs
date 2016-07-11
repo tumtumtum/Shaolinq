7b2c5ffd65379e04944da589adc3796a4d7bef0d
Thong Nguyen <tumtumtum@gmail.com>
2016-07-11 23:29:26 +0100

    Support dynamic assembly resolution of Microsoft.CodeAnalysis libraries

--- src/Shaolinq.AsyncRewriter/AsyncRewriterTask.cs
+++ src/Shaolinq.AsyncRewriter/AsyncRewriterTask.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Build.Framework;
 
 namespace Shaolinq.AsyncRewriter
@@ -15,9 +18,57 @@ namespace Shaolinq.AsyncRewriter
 		public ITaskItem[] Assemblies { get; set; }
 
 		private readonly Rewriter rewriter;
+		
+		private static IEnumerable<string> GetDlls(string parentDirectory)
+		{
+			foreach (var file in Directory.GetFiles(parentDirectory, "*.dll"))
+			{
+				yield return file;
+			}
+
+			foreach (var directory in Directory.GetDirectories(parentDirectory))
+			{
+				foreach (var file in GetDlls(Path.Combine(parentDirectory, directory)))
+				{
+					yield return file;
+				}
+			}
+		}
 
 		public AsyncRewriterTask()
 		{
+			var assemblyPath = this.GetType().Assembly.Location;
+
+			var components =
+				Path.GetDirectoryName(assemblyPath)
+				.Split(Path.DirectorySeparatorChar)
+				.Reverse()
+				.SkipWhile(c => !c.Equals("packages", StringComparison.InvariantCultureIgnoreCase))
+				.Reverse()
+				.ToList();
+
+			var path = string.Join(Path.DirectorySeparatorChar.ToString(), components);
+
+			if (path != "")
+			{
+				var pathsByFileName = GetDlls(path).ToDictionary(Path.GetFileName, c => c);
+
+				AppDomain.CurrentDomain.AssemblyResolve += (sender, e) =>
+				{
+					var fileName = e.Name.Split(',')[0];
+
+					if (pathsByFileName.TryGetValue(fileName, out path))
+					{
+						if (File.Exists(path))
+						{
+							return Assembly.LoadFrom(path);
+						}
+					}
+
+					return null;
+				};
+			}
+
 			this.rewriter = new Rewriter();
 		}
 
