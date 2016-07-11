bca62fdfb2e1589a859ccf924d3e091b65ab5d9b
Thong Nguyen <tumtumtum@gmail.com>
2016-07-11 20:05:57 +0100

    Added nuspec files for Shaolinq.AsyncRewriter

---
+++ nuspec/RewriteAsyncAttribute.cs.pp
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2016 Thong Nguyen (tumtumtum@gmail.com)
+
+using System;
+using System.Reflection;
+
+namespace $rootnamespace$
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	internal class RewriteAsyncAttribute
+		: Attribute
+	{
+		private MethodAttributes MethodAttributes { get; private set; }
+
+		public RewriteAsyncAttribute(MethodAttributes methodAttributes = default(MethodAttributes))
+		{
+			this.MethodAttributes = methodAttributes;
+		}
+	}
+}
