9a3f442e6188b42f14004571ee63eebe510cb3f3
Thong Nguyen <tumtumtum@gmail.com>
2016-08-09 00:36:57 +0100

    Added missing ConditionalAccess.cs

---
+++ tests/Shaolinq.AsyncRewriter.Tests/ConditionalAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Shaolinq.AsyncRewriter.Tests
+{
+	[TestFixture]
+	public class ConditionalAccess
+	{
+		public class Foo
+		{
+			[RewriteAsync]
+			public void Bar()
+			{	
+			}
+		}
+
+		public class Car
+		{
+			public void Toot(bool value)
+			{
+			}
+		}
+
+		public Foo foo;
+
+		[RewriteAsync]
+		public void Test()
+		{
+			this.foo?.Bar();
+		}
+	}
+}
