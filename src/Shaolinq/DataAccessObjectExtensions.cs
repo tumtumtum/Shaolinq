ceef1ab31cd7a6f2b1437bf04e7db317c15248d7
Thong Nguyen <tumtumtum@gmail.com>
2017-03-15 13:43:01 +0000

    Made a few more internal methods public to allow access from reused assemblies

--- src/Shaolinq/DataAccessObjectExtensions.cs
+++ src/Shaolinq/DataAccessObjectExtensions.cs
@@ -1,39 +1,15 @@
 ﻿// Copyright (c) 2007-2016 Thong Nguyen (tumtumtum@gmail.com)
 
 using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Platform.Reflection;
 using Shaolinq.TypeBuilding;
 
 namespace Shaolinq
 {
 	public static class DataAccessObjectExtensions
 	{
-		internal static P AddToCollection<P, C>(P parent, Func<P, RelatedDataAccessObjects<C>> getChildren, C child, int version)
-			where P : DataAccessObject
-			where C : DataAccessObject
-		{
-			if (parent == null)
-			{
-				return null;
-			}
-
-			getChildren(parent).Add(child, version);
-
-			return parent;
-		}
-		
-		internal static T Include<T, U>(this T obj, Expression<Func<T, U>> include)
-		{
-			// ReSharper disable SuspiciousTypeConversion.Global
-			return obj;
-			// ReSharper restore SuspiciousTypeConversion.Global
-		}
-
-		internal static IDataAccessObjectInternal ToObjectInternal(this DataAccessObject value)
+		public static IDataAccessObjectInternal ToObjectInternal(this DataAccessObject value)
 		{
 			// ReSharper disable SuspiciousTypeConversion.Global
 			return value as IDataAccessObjectInternal;
--- src/Shaolinq/DataAccessObjectHelpersInternal.cs
+++ src/Shaolinq/DataAccessObjectHelpersInternal.cs
@@ -7,8 +7,29 @@ using Platform;
 
 namespace Shaolinq
 {
-	public class DataAccessObjectHelpersInternal
+	public static class DataAccessObjectHelpersInternal
 	{
+		public static P AddToCollection<P, C>(P parent, Func<P, RelatedDataAccessObjects<C>> getChildren, C child, int version)
+			where P : DataAccessObject
+			where C : DataAccessObject
+		{
+			if (parent == null)
+			{
+				return null;
+			}
+
+			getChildren(parent).Add(child, version);
+
+			return parent;
+		}
+
+		public static T Include<T, U>(this T obj, Expression<Func<T, U>> include)
+		{
+			// ReSharper disable SuspiciousTypeConversion.Global
+			return obj;
+			// ReSharper restore SuspiciousTypeConversion.Global
+		}
+
 		public static Expression GetPropertyValueExpressionFromPredicatedDeflatedObject<T, U>(T obj, string propertyPath)
 			where T : DataAccessObject
 		{
