5d20832fe3d795388b2454a38255955e425dd931
Thong Nguyen <tumtumtum@gmail.com>
2017-05-12 10:37:06 +0100

    Add support for auto0-detecting file or connection string in SqliteConfiguration

--- src/Shaolinq.Sqlite/SqliteConfiguration.cs
+++ src/Shaolinq.Sqlite/SqliteConfiguration.cs
@@ -1,37 +1,40 @@
 // Copyright (c) 2007-2017 Thong Nguyen (tumtumtum@gmail.com)
 
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using Shaolinq.Persistence;
 
 namespace Shaolinq.Sqlite
 {
 	public static class SqliteConfiguration
 	{
-		public static DataAccessModelConfiguration Create(string connectionString)
+		private static readonly Regex connectionStringRegex = new Regex(@"\s*Data Source\s*=", RegexOptions.Compiled);
+		
+		public static DataAccessModelConfiguration Create(string connectionStringOrFileName, string categories = null, bool useMonoData = false)
 		{
+			string fileName = null;
+			string connectionString = null;
+
+			if (connectionStringRegex.IsMatch(connectionStringOrFileName) || connectionStringOrFileName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+			{
+				connectionString = connectionStringOrFileName;
+			}
+			else
+			{
+				fileName = connectionStringOrFileName;
+			}
+			
 			return new DataAccessModelConfiguration
 			{
 				SqlDatabaseContextInfos = new List<SqlDatabaseContextInfo>
 				{
 					new SqliteSqlDatabaseContextInfo
 					{
-						ConnectionString = connectionString
-					},
-				}
-			};
-		}
-
-		public static DataAccessModelConfiguration Create(string fileName, string categories, bool useMonoData = false)
-		{
-			return new DataAccessModelConfiguration
-			{
-				SqlDatabaseContextInfos = new List<SqlDatabaseContextInfo>
-				{
-					new SqliteSqlDatabaseContextInfo()
-					{
 						Categories = categories,
 						FileName = fileName,
-						UseMonoData = useMonoData
+						UseMonoData = useMonoData,
+						ConnectionString = connectionString
 					},
 				}
 			};
