5232002a11ec4aa1158f193a8c41208f91543697
Sam Cook <github@sam.net.nz>
2017-11-02 18:34:46 +0000

    Add test case for picking up Async methods declared in base classes or interfaces

--- tests/Shaolinq.AsyncRewriter.Tests/AsyncRewriterTests.cs
+++ tests/Shaolinq.AsyncRewriter.Tests/AsyncRewriterTests.cs
@@ -27,6 +27,7 @@ namespace Shaolinq.AsyncRewriter.Tests
 			yield return GetTestCaseData("Expression body", "TestExpressionBody.cs");
 			yield return GetTestCaseData("Attribute on class", "TestAttributeOnClass.cs");
 			yield return GetTestCaseData("Language features", "LanguageFeatures.cs");
+			yield return GetTestCaseData("Nested async methods", "NestedAsync.cs");
 		}
 
 		private static TestCaseData GetTestCaseData(string name, params string[] inputFiles)
