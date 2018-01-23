6ec7e06265867dc7bc20dc2df19e4df0af3a34d5
Thong Nguyen <tumtumtum@gmail.com>
2018-01-23 10:40:14 +0000

    Added missing ExecuteReaderContext

---
+++ src/Shaolinq/Persistence/ExecuteReaderContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Data;
+
+namespace Shaolinq.Persistence
+{
+	public class ExecuteReaderContext : IDisposable
+	{
+		public IDbCommand DbCommand { get; private set; }
+		public IDataReader DataReader { get; private set; }
+		
+		public ExecuteReaderContext(IDataReader dataReader, IDbCommand dbCommand)
+		{
+			this.DataReader = dataReader;
+			this.DbCommand = dbCommand;
+		}
+
+		public void Dispose()
+		{
+			this.DataReader?.Dispose();
+			this.DbCommand?.Dispose();
+
+			this.DataReader = null;
+			this.DbCommand = null;
+		}
+	}
+}
