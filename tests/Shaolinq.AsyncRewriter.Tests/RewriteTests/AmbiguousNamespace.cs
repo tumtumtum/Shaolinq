a0ffc0887cc85c9ce474dc871dc9cc00beb4caad
Sam Cook <github@sam.net.nz>
2019-09-05 14:34:08 +0100

    Add test sample for async rewriter ambiguous namespace

---
+++ tests/Shaolinq.AsyncRewriter.Tests/RewriteTests/AmbiguousNamespace.cs
@@ -0,0 +1,34 @@
+﻿using Other;
+using Shaolinq.AsyncRewriter.Tests;
+
+namespace Something.AsyncRewriter.Tests.RewriteTests
+{
+	public partial class AmbiguousNamespace
+	{
+		[RewriteAsync]
+		public static int Main()
+		{
+			var otherFoo = new Foo();
+			var somethingOtherFoo = new Something.Other.Foo();
+
+			TestOtherFoo(otherFoo);
+			TestSomethingOtherFoo(somethingOtherFoo);
+
+			return 0;
+		}
+
+		[RewriteAsync]
+		public static void TestOtherFoo(Foo foo) // Other.Foo
+		{
+			foo.Bar();
+			//foo.OtherBar();
+		}
+
+		[RewriteAsync]
+		public static void TestSomethingOtherFoo(Other.Foo foo) // Something.Other.Foo
+		{
+			foo.Bar();
+			//foo.SomethingOtherBar();
+		} 
+	}
+}
